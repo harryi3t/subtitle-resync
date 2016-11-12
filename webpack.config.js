007a94893f453da2b1783390d39317457285da76
harryi3t <66hari@gmail.com>
2016-11-13 00:45:00 +0530

    adds separate config for prod and development Disabled the hot loading in prod. Since i will be using this as static app without node and publish on github, so would need to generate the bundle.js files everytime using commandline

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-  devtool: 'eval',
+var prodConfig = require('./webpack.prod.config.js');
+
+Object.assign(prodConfig, {
   entry: [
     'react-hot-loader/patch',
     'webpack-hot-middleware/client',
     './src/index'
   ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
-};
+})
+
+module.exports = prodConfig;
---
+++ webpack.prod.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
